268bc09a3aa06752c13d65f23c0952a2165417aa
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2018-04-24 17:59:04 +0600

    Добавил генерацию жедел өткен шақ

--- generate_verb_forms.py
+++ generate_verb_forms.py
@@ -9,6 +9,7 @@ def generate_verb_forms(word_and_tags: tuple) -> list:
     """
     result = []
     result += generate_negative_verb(word_and_tags)
+    result += generate_all_zhedel_otken_shaq(word_and_tags)
 
     return result
 
@@ -43,4 +44,89 @@ def generate_negative_verb(word_and_tags: tuple) -> list:
     new_tags["negativity"] = "<neg>"
 
     result.append((new_word, new_tags))
+    return result
+
+
+def generate_all_zhedel_otken_shaq(word_and_tags: tuple) -> list:
+    """функция, генерирующая жедел өткен шақ во всех лицах
+    Основа глагола + -ды/-дi/-ты/-тi + краткие личные окончания
+    глухие -ты/-тi
+    неглухие -ды/-дi
+    """
+    word, tags = word_and_tags
+    result = []
+
+    if word[-1] in voiceless_consonant:
+        new_word = add_affix_choosing_hard_or_soft(word,
+                                                   hard_affix="ты",
+                                                   soft_affix="ті")
+    else:
+        new_word = add_affix_choosing_hard_or_soft(word,
+                                                   hard_affix="ды",
+                                                   soft_affix="ді")
+
+    # 3 лицо ед. и мн.число генерируется вначале
+    # Потом на его основе генерируются остальные лица
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p3>"
+    new_tags["plurality"] = "<sp>"
+    result.append((new_word, new_tags))
+
+    # 1 лицо ед.число
+    tmp_new_word = new_word + 'м'
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p1>"
+    new_tags["plurality"] = "<sg>"
+    result.append((tmp_new_word, new_tags))
+
+    # 1 лицо мн.число
+    tmp_new_word = add_affix_choosing_hard_or_soft(new_word,
+                                                   hard_affix='қ',
+                                                   soft_affix='к')
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p1>"
+    new_tags["plurality"] = "<pl>"
+    result.append((tmp_new_word, new_tags))
+
+    # 2 лицо ед.число
+    tmp_new_word = new_word + 'ң'
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p2>"
+    new_tags["plurality"] = "<sg>"
+    result.append((tmp_new_word, new_tags))
+
+    # 2 лицо мн.число
+    tmp_new_word = add_affix_choosing_hard_or_soft(new_word,
+                                                   hard_affix='ңдар',
+                                                   soft_affix='ңдер')
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p2>"
+    new_tags["plurality"] = "<pl>"
+    result.append((tmp_new_word, new_tags))
+
+    # 2 лицо (вежливое) ед.число
+    tmp_new_word = add_affix_choosing_hard_or_soft(new_word,
+                                                   hard_affix='ңыз',
+                                                   soft_affix='ңіз')
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p2_2>"
+    new_tags["plurality"] = "<sg>"
+    result.append((tmp_new_word, new_tags))
+
+    # 2 лицо (вежливое) мн.число
+    tmp_new_word = add_affix_choosing_hard_or_soft(new_word,
+                                                   hard_affix='ңыздар',
+                                                   soft_affix='ңіздер')
+    new_tags = tags.copy()
+    new_tags["tense"] = "<past><zhedel-otken>"
+    new_tags["person"] = "<p2_2>"
+    new_tags["plurality"] = "<pl>"
+    result.append((tmp_new_word, new_tags))
+
     return result
