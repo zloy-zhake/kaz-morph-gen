0ec56c9c132690cd9e4b69e3976cadf5aa777a32
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2018-05-09 17:03:54 +0600

    Тест для генерации множественного числа

---
+++ tests/test_common_forms.py
@@ -0,0 +1,47 @@
+from common_forms import generate_plural
+from common_forms import generate_all_possessives
+
+
+def generate_results_to_test(func, words_to_test: tuple) -> list:
+    """Функция, генерирующая результаты (слова без тегов) для тестов.
+    Нужна чтобы не повторять одно и то же в каждом тесте.
+    """
+    # actual_results содержит сгенерированные слова и dict с тегами
+    actual_results = []
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+
+    # заполняем actual_results
+    for item in words_to_test:
+        actual_results += func(word_and_tags=item)
+
+    # заполняем results_to_test
+    for item in actual_results:
+        results_to_test.append(item[0])
+
+    return results_to_test
+
+
+def test_generate_plural():
+    words_to_test = [("қаз", dict()),
+                     ("көз", dict()),
+                     ("ата", dict()),
+                     ("әже", dict()),
+                     ("ат", dict()),
+                     ("мектеп", dict())]
+    expected_results = ["қаздар",
+                        "көздер",
+                        "аталар",
+                        "әжелер",
+                        "аттар",
+                        "мектептер"]
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_plural,
+                                               words_to_test=words_to_test)
+
+    assert results_to_test == expected_results
+
+
+def test_generate_all_possessives():
+    pass
