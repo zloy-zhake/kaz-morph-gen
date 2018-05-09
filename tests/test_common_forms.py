b38f8c99a031b477e59bfa16c21c68a2965da854
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2018-05-09 19:06:32 +0600

    Добавил тесты для всех падежей

--- tests/test_common_forms.py
+++ tests/test_common_forms.py
@@ -1,6 +1,13 @@
 import pytest
 from common_forms import generate_plural
 from common_forms import generate_all_possessives
+from common_forms import generate_atau_septik
+from common_forms import generate_ilik_septik
+from common_forms import generate_barys_septik
+from common_forms import generate_tabys_septik
+from common_forms import generate_zhatys_septik
+from common_forms import generate_shygys_septik
+from common_forms import generate_komektes_septik
 
 
 def generate_results_to_test(func, words_to_test: tuple) -> list:
@@ -40,7 +47,6 @@ def test_generate_plural():
     results_to_test = []
     results_to_test = generate_results_to_test(func=generate_plural,
                                                words_to_test=words_to_test)
-
     assert results_to_test == expected_results
 
 
@@ -69,7 +75,113 @@ def test_generate_all_possessives():
     results_to_test = []
     results_to_test = generate_results_to_test(func=generate_all_possessives,
                                                words_to_test=words_to_test)
-    for item in results_to_test:
-        print(item)
+    assert results_to_test == expected_results
 
+
+def test_generate_atau_septik():
+    words_to_test = [("мектеп", dict()),
+                     ("адам", dict()),
+                     ("бала", dict())]
+    expected_results = ["мектеп",
+                        "адам",
+                        "бала"]
+
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_atau_septik,
+                                               words_to_test=words_to_test)
+    assert results_to_test == expected_results
+
+
+@pytest.mark.skip(reason="Исправить ошибку")
+def test_generate_ilik_septik():
+    words_to_test = [("қазақстан", dict()),
+                     ("жер", dict()),
+                     ("мектеп", dict())]
+    expected_results = ["қазақстанның",
+                        "жердiң",
+                        "мектептiң"]
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_ilik_septik,
+                                               words_to_test=words_to_test)
+    assert results_to_test == expected_results
+
+
+def test_generate_barys_septik():
+    words_to_test = [("адам", dict()),
+                     ("кiтап", dict()),
+                     ("үй", dict()),
+                     ("ленинград", dict()),
+                     ("балтам", {"person": "<px1sg>"}),
+                     ("инесi", {"person": "<px3sp>"})]
+    expected_results = ["адамға",
+                        "кiтапқа",
+                        "үйге",
+                        "ленинградқа",
+                        "балтама",
+                        "инесiне"]
+
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_barys_septik,
+                                               words_to_test=words_to_test)
+    assert results_to_test == expected_results
+
+
+def test_generate_tabys_septik():
+    words_to_test = [("бала", dict()),
+                     ("терезе", dict())]
+    expected_results = ["баланы",
+                        "терезенi"]
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_tabys_septik,
+                                               words_to_test=words_to_test)
+    assert results_to_test == expected_results
+
+
+def test_generate_zhatys_septik():
+    words_to_test = [("завод", dict()),
+                     ("мектеп", dict()),
+                     ("сара", dict()),
+                     ("казан", dict()),
+                     ("әкесi", {"person": "<px3sp>"})]
+    expected_results = ["заводта",
+                        "мектепте",
+                        "сарада",
+                        "казанда",
+                        "әкесiнде"]
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_zhatys_septik,
+                                               words_to_test=words_to_test)
+    assert results_to_test == expected_results
+
+
+def test_generate_shygys_septik():
+    words_to_test = [("қар", dict()),
+                     ("қала", dict()),
+                     ("арқалық", dict())]
+    expected_results = ["қардан",
+                        "қаладан",
+                        "арқалықтан"]
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_shygys_septik,
+                                               words_to_test=words_to_test)
+    assert results_to_test == expected_results
+
+
+def test_generate_komektes_septik():
+    words_to_test = [("тарақ", dict()),
+                     ("сабын", dict()),
+                     ("iнiм", dict())]
+    expected_results = ["тарақпен",
+                        "сабынмен",
+                        "iнiммен"]
+    # results_to_test содержит только сгенерированные слова
+    results_to_test = []
+    results_to_test = generate_results_to_test(func=generate_komektes_septik,
+                                               words_to_test=words_to_test)
     assert results_to_test == expected_results
